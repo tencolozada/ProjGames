e0d6dcaea59157e754c823e3245a99b1c3ecdbdf
tencolozada <83680501+tencolozada@users.noreply.github.com>
2023-01-02 11:54:09 +0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "< br  / > What type of tag is this?",
+    answer: "Break tag",
+    options: [
+      "Break tag",
+      "A broken one",
+      "An opening tag",
+      "close Tag"
+    ]
+  },
+
+     {
+    numb: 7,
+    question: "What is the difference between an opening tag and a closing tag?",
+    answer: "There is no difference",
+    options: [
+      "There is no difference",
+      "Closing tag has a / in front",
+      "Opening tag has a / in front",
+      "close Tag"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "< body > Is this an opening tag or a closing tag?",
+    answer: "Opening",
+    options: [
+      "Opening",
+      "Closing"
+    ]
+  },
+  
+      {
+    numb: 9,
+    question: "Where is the meta tag only found?",
+    answer: "The home page",
+    options: [
+      "The home page",
+      "The last page",
+      "The second page"
+    ]
+  },
+
+     {
+    numb: 10,
+    question: "What is an element that does not have a closing tag called?",
+    answer: "Empty element",
+    options: [
+      "Empty element",
+      "Tag",
+      "Closed element",
+    ]
+  },
+
+
+
+
+];
